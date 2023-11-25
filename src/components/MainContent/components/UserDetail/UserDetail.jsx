64e82cf12c946ba329546950184ec7311cd8d3e5
tictools <jaumevillarreal@gmail.com>
2023-11-25 12:36:33 +0100

    feat: add first iteration for main content component

---
+++ src/components/MainContent/components/UserDetail/UserDetail.jsx
@@ -0,0 +1,9 @@
+import styles from "./UserDetail.module.css";
+
+export const UserDetail = () => {
+  return (
+    <section className={styles["wrapper"]}>
+      <h2 className={styles["header"]}>User Detail</h2>
+    </section>
+  );
+};
