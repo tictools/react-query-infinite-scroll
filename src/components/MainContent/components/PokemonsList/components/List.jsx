b469f387513a15f7b92b88f26817e2a6d62d4595
tictools <jaumevillarreal@gmail.com>
2023-11-26 07:20:43 +0100

    feat: add conditional chaining to properly render list elements

--- src/components/MainContent/components/PokemonsList/components/List.jsx
+++ src/components/MainContent/components/PokemonsList/components/List.jsx
@@ -3,7 +3,7 @@ import styles from "../PokemonsList.module.css";
 export const List = ({ pokemons = [] }) => {
   return (
     <ul className={styles["list"]}>
-      {pokemons.map((pokemon) => (
+      {pokemons?.map((pokemon) => (
         <li className={styles["list__item"]} key={pokemon.id}>
           {pokemon.name}
         </li>
