c05eeaf5f67a294bc6e702bc9f73016199edd564
tictools <jaumevillarreal@gmail.com>
2023-11-26 07:12:31 +0100

    feat: define default value for pokemons prop

--- src/components/MainContent/components/PokemonsList/components/List.jsx
+++ src/components/MainContent/components/PokemonsList/components/List.jsx
@@ -1,9 +1,9 @@
 import styles from "../PokemonsList.module.css";
 
-export const List = ({ pokemons }) => {
+export const List = ({ pokemons = [] }) => {
   return (
     <ul className={styles["list"]}>
-      {pokemons?.map((pokemon) => (
+      {pokemons.map((pokemon) => (
         <li className={styles["list__item"]} key={pokemon.id}>
           {pokemon.name}
         </li>
