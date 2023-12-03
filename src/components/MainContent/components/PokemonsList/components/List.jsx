7fc03994a618c0ea80eaccee045ced0272a7e4ca
tictools <jaumevillarreal@gmail.com>
2023-12-03 07:38:29 +0100

    feat: implement infinite scroll and intersection observer

--- src/components/MainContent/components/PokemonsList/components/List.jsx
+++ src/components/MainContent/components/PokemonsList/components/List.jsx
@@ -1,13 +1,38 @@
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
 import styles from "../PokemonsList.module.css";
+import { usePokemonData } from "../hooks/usePokemonData";
 
-export const List = ({ pokemons = [] }) => {
+export const List = () => {
+  const { fetchNextPage, hasNextPage, pokemons } = usePokemonData();
+
+  const { ref: inViewRef, inView } = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      fetchNextPage();
+    }
+  }, [fetchNextPage, inView]);
+
   return (
     <ul className={styles["list"]}>
       {pokemons?.map((pokemon) => (
-        <li className={styles["list__item"]} key={pokemon.id}>
+        <li className={styles["list__item"]} key={pokemon.name}>
           {pokemon.name}
         </li>
       ))}
+      <div
+        ref={inViewRef}
+        style={{
+          backgroundColor: "#FCB667",
+          borderRadius: "0.25rem",
+          height: "auto",
+          width: "100%",
+          textAlign: "center",
+        }}
+      >
+        {hasNextPage ? "Fetching Pokemons..." : "No more pokemons to fetch"}
+      </div>
     </ul>
   );
 };
