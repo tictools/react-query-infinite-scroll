4e7b24b87b6cd6c8a25236353d8aa95898a6b83a
tictools <jaumevillarreal@gmail.com>
2023-12-17 20:00:54 +0100

    fix: remove log

--- src/ui/MainContent/components/PokemonsList/components/ListItem.jsx
+++ src/ui/MainContent/components/PokemonsList/components/ListItem.jsx
@@ -5,20 +5,11 @@ import styles from "../PokemonsList.module.css";
 export const ListItem = ({ name, index }) => {
   const getCurrent = useStore((state) => state.getCurrent);
   const selectedIndex = useStoreSelectorBy("currentIndex");
-  console.log(
-    "🚀 ~ file: ListItem.jsx:8 ~ ListItem ~ selectedIndex:",
-    selectedIndex
-  );
 
   const itemClassName =
     selectedIndex === index
       ? `${styles["list__item"]} ${styles["list__item--selected"]}`
       : styles["list__item"];
-  console.log(
-    "🚀 ~ file: ListItem.jsx:14 ~ ListItem ~ itemClassName:",
-    itemClassName
-  );
-
   const handleClick = () => {
     getCurrent(index);
   };
