17ef6312ea860077a59429666cf9ec2f4bd2b599
tictools <jaumevillarreal@gmail.com>
2023-12-23 19:31:41 +0100

    feat: refine autoScroll hook

---
+++ src/ui/MainContent/hooks/useAutoScroll.jsx
@@ -0,0 +1,20 @@
+import { useCallback } from "react";
+// import { usePokemonsData } from "./usePokemonsData";
+import { useStoreSelectorBy } from "./useStoreSelectorBy";
+
+export const useAutoScroll = () => {
+  // const { currentLength } = usePokemonsData();
+  const elementsRef = useStoreSelectorBy("elementsRef");
+  const currentIndex = useStoreSelectorBy("currentIndex");
+
+  const handleAutoScroll = useCallback(() => {
+    if (!elementsRef[currentIndex]?.current) return;
+
+    elementsRef[currentIndex].current.scrollIntoView({
+      behavior: "smooth",
+      block: "end",
+    });
+  }, [currentIndex, elementsRef]);
+
+  return { handleAutoScroll };
+};
