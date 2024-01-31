ba6415d5387fa84e3ca6e8ceba5114fe23446edd
tictools <jaumevillarreal@gmail.com>
2024-01-31 19:43:42 +0100

    feat: add first iteration for artwork card styles

--- src/ui/MainContent/components/PokemonDetail/components/Type.jsx
+++ src/ui/MainContent/components/PokemonDetail/components/Type.jsx
@@ -1,3 +1,5 @@
+import styles from "../PokemonDetail.module.css";
+
 export const Type = ({ type }) => {
-  return <li>{<p>{type} </p>}</li>;
+  return <li>{<p className={styles["artwork__type"]}>{type} </p>}</li>;
 };
--- src/ui/MainContent/components/PokemonDetail/components/TypesList.jsx
+++ src/ui/MainContent/components/PokemonDetail/components/TypesList.jsx
@@ -1,8 +1,9 @@
+import styles from "../PokemonDetail.module.css";
 import { Type } from "./Type";
 
 export const TypesList = ({ types }) => {
   return (
-    <ul>
+    <ul className={styles["artwork__types"]}>
       {types.map((type) => (
         <Type key={type} type={type} />
       ))}
