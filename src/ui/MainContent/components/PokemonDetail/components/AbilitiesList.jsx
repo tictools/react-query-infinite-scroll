b4cc12f40a925650363ef550d34d9dd970ca7f41
tictools <jaumevillarreal@gmail.com>
2024-03-12 15:23:01 +0100

    feat: define new ability layout

--- src/ui/MainContent/components/PokemonDetail/components/AbilitiesList.jsx
+++ src/ui/MainContent/components/PokemonDetail/components/AbilitiesList.jsx
@@ -1,8 +1,10 @@
 import { Ability } from "./Ability";
 
+import styles from "../PokemonDetail.module.css";
+
 export const AbilitiesList = ({ abilities }) => {
   return (
-    <ul>
+    <ul className={styles["abilities__container"]}>
       {abilities.map(({ ability, is_hidden }) => (
         <Ability key={ability.name} name={ability.name} isHidden={is_hidden} />
       ))}
