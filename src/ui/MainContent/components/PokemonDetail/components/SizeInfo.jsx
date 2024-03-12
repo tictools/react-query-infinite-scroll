4f63223446d1ea090aa1d2be7a973466b72f15e8
tictools <jaumevillarreal@gmail.com>
2024-03-12 17:05:42 +0100

    feat: delete SizeData component and redefine size info layout

--- src/ui/MainContent/components/PokemonDetail/components/SizeInfo.jsx
+++ src/ui/MainContent/components/PokemonDetail/components/SizeInfo.jsx
@@ -1,13 +1,28 @@
-import { SizeHeight } from "./SizeHeight";
-import { SizeWeight } from "./SizeWeight";
-
 import styles from "../PokemonDetail.module.css";
+import { IconHeight } from "./IconHeight";
+import { IconWeight } from "./IconWeight";
 
 export const SizeInfo = ({ height, weight }) => {
+  const iconLeftClassName = `${styles["size__item-icon"]} ${styles["size__item-info--left"]}`;
+  const iconRightClassName = `${styles["size__item-icon"]} ${styles["size__item-info--right"]}`;
+
+  const valueLeftClassName = `${styles["size__item-value"]} ${styles["size__item-info--left"]}`;
+  const valueRightClassName = `${styles["size__item-value"]} ${styles["size__item-info--right"]}`;
+
   return (
     <ul className={styles["size__container"]}>
-      <SizeHeight measureKey="height" value={height} />
-      <SizeWeight measureKey="weight" value={weight} />
+      <li className={`${styles["size__item"]} ${styles["size__item--left"]}`}>
+        <span className={iconLeftClassName}>
+          <IconHeight color="#fff" size={20} />
+        </span>
+        <span className={valueRightClassName}>{height}</span>
+      </li>
+      <li className={`${styles["size__item"]} ${styles["size__item--right"]}`}>
+        <span className={valueLeftClassName}>{weight}</span>
+        <span className={iconRightClassName}>
+          <IconWeight color="#fff" size={20} />
+        </span>
+      </li>
     </ul>
   );
 };
